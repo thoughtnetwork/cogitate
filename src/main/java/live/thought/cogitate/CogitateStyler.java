7cad60923bcd60259990afc26553ac308991e08d
Phil Grim <pagrim@mistiqtech.com>
2019-04-23 17:45:49 -0400

    Added copyright notice.  Added style for list items in the nav bar.

--- src/main/java/live/thought/cogitate/CogitateStyler.java
+++ src/main/java/live/thought/cogitate/CogitateStyler.java
@@ -1,3 +1,22 @@
+/*
+ * cogitate - Blockchain browser for the Thought Network.
+ * 
+ * Copyright (c) 2018 - 2019, Thought Network LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package live.thought.cogitate;
 
 public abstract class CogitateStyler
@@ -39,6 +58,9 @@ public abstract class CogitateStyler
       "nav ul {" +
       "  list-style-type: none;" +
       "  padding: 0;" +
+      "}" +
+      "nav li a {" + 
+      "  display: block;" + 
       "}" +
       "article {" +
       "  -webkit-flex: 3;" +
