f9350ae6f7d90e468975f43d5695522ded9ae1f5
Sam Heybey <sam@heybey.org>
2019-09-04 21:03:17 -0400

    simplify time rendering script

--- src/main/resources/static/render_time.js
+++ src/main/resources/static/render_time.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   'use strict';
   document.querySelectorAll('time').forEach(function (time) {
-    var date = new Date(time.dateTime);
-    time.textContent = date.toLocaleString();
+    time.textContent = new Date(time.dateTime).toLocaleString();
   });
 });
